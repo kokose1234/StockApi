bf51bb5f023ba2e4fa3dbd62f3a713af993541b4
SLFCG <kokose1234@naver.com>
2021-05-28 04:19:55 +0900

    키워드 추가 제거api 추가

--- src/Controllers/NewsController.cs
+++ src/Controllers/NewsController.cs
@@ -19,6 +19,7 @@ using System.Net;
 using System.Text;
 using HtmlAgilityPack;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using StockApi.Data;
 
 namespace StockApi.Controllers
@@ -27,60 +28,80 @@ namespace StockApi.Controllers
     [ApiController]
     public class NewsController : ControllerBase
     {
-        [HttpGet]
-        public IActionResult Get([FromQuery(Name = "key")] string apiKey, [FromQuery(Name = "searchWords")] string input)
+        private readonly IList<string> keywords;
+
+        private NewsController()
         {
-            if (apiKey == Setting.Value.ApiKey)
+            if (!System.IO.File.Exists("keywords.json"))
+                System.IO.File.WriteAllText("keywords.json", JsonConvert.SerializeObject(new List<string>()));
+
+            keywords = JsonConvert.DeserializeObject<List<string>>(System.IO.File.ReadAllText("keywords.json"));
+        }
+
+        [HttpGet("getKeywords")]
+        public IActionResult GetKeywords([FromQuery] string apiKey) => StatusCode(200, keywords);
+
+        [HttpPost("updateKeywords")]
+        public IActionResult UpdateKeywords([FromQuery] string keywordList)
+        {
+            keywords.Clear();
+
+            foreach (var keyword in keywordList.Split(',')) keywords.Add(keyword);
+
+            return StatusCode(200);
+        }
+
+        [HttpGet("getNews")]
+        public IActionResult GetNews()
+        {
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+            using var wc = new WebClient
             {
-                Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
-
-                using var wc = new WebClient
+                Encoding = Encoding.GetEncoding(51949),
+                Headers =
                 {
-                    Encoding = Encoding.GetEncoding(51949),
-                    Headers =
-                    {
-                        ["Accept"] =
-                            "application/x-ms-application, image/jpeg, application/xaml+xml, image/gif, image/pjpeg, application/x-ms-xbap, application/x-shockwave-flash, application/vnd.ms-excel, application/vnd.ms-powerpoint, application/msword, */*",
-                        ["User-Agent"] =
-                            "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; WOW64; Trident/4.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; MDDC)"
-                    }
-                };
-                var document = new HtmlDocument();
-                var newsList = new List<News>();
-
-                document.LoadHtml(wc.DownloadString("https://finance.naver.com/news/mainnews.nhn"));
-
-                for (var a = 1;; a++)
-                {
-                    var title = document.DocumentNode.SelectSingleNode(
-                        $"/html[1]/body[1]/div[3]/div[1]/div[2]/div[1]/div[2]/ul[1]/li[{a}]/dl[1]/dd[1]/a[1]");
-                    var content = document.DocumentNode.SelectSingleNode(
-                        $"/html[1]/body[1]/div[3]/div[1]/div[2]/div[1]/div[2]/ul[1]/li[{a}]/dl[1]/dd[2]");
-                    var thumbnail = document.DocumentNode.SelectSingleNode(
-                        $"/html[1]/body[1]/div[3]/div[1]/div[2]/div[1]/div[2]/ul[1]/li[{a}]/dl[1]/dt[1]/a[1]/img[1]");
-
-                    title ??= document.DocumentNode.SelectSingleNode(
-                        $"/html[1]/body[1]/div[3]/div[1]/div[2]/div[1]/div[2]/ul[1]/li[{a}]/dl[1]/dt[1]/a[1]");
-                    content ??= document.DocumentNode.SelectSingleNode(
-                        $"/html[1]/body[1]/div[3]/div[1]/div[2]/div[1]/div[2]/ul[1]/li[{a}]/dl[1]/dd[1]");
-
-                    if (title == null | content == null) break;
-
-                    if (input.Split(',').Any(keyword => title.InnerText.Contains(keyword) || content.InnerText.Contains(keyword)))
-                    {
-                        newsList.Add(new News
-                        {
-                            Title = title.InnerText,
-                            Url = $"https://finance.naver.com/{title.GetAttributeValue("href", "")}",
-                            ThumbnailUrl = thumbnail == null ? "" : thumbnail.GetAttributeValue("src", "")
-                        });
-                    }
+                    ["Accept"] =
+                        "application/x-ms-application, image/jpeg, application/xaml+xml, image/gif, image/pjpeg, application/x-ms-xbap, application/x-shockwave-flash, application/vnd.ms-excel, application/vnd.ms-powerpoint, application/msword, */*",
+                    ["User-Agent"] =
+                        "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; WOW64; Trident/4.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; MDDC)"
                 }
+            };
+            var document = new HtmlDocument();
+            var newsList = new List<News>();
 
-                return StatusCode(200, new NewsInfoDto() {NewsList = newsList, UpdateDate = $"{DateTime.Now.AddHours(9):yyyy-MM-dd}"});
+            document.LoadHtml(wc.DownloadString("https://finance.naver.com/news/mainnews.nhn"));
+
+            for (var a = 1;; a++)
+            {
+                var title = document.DocumentNode.SelectSingleNode(
+                    $"/html[1]/body[1]/div[3]/div[1]/div[2]/div[1]/div[2]/ul[1]/li[{a}]/dl[1]/dd[1]/a[1]");
+                var content = document.DocumentNode.SelectSingleNode(
+                    $"/html[1]/body[1]/div[3]/div[1]/div[2]/div[1]/div[2]/ul[1]/li[{a}]/dl[1]/dd[2]");
+                var thumbnail = document.DocumentNode.SelectSingleNode(
+                    $"/html[1]/body[1]/div[3]/div[1]/div[2]/div[1]/div[2]/ul[1]/li[{a}]/dl[1]/dt[1]/a[1]/img[1]");
+
+                title ??= document.DocumentNode.SelectSingleNode(
+                    $"/html[1]/body[1]/div[3]/div[1]/div[2]/div[1]/div[2]/ul[1]/li[{a}]/dl[1]/dt[1]/a[1]");
+                content ??= document.DocumentNode.SelectSingleNode(
+                    $"/html[1]/body[1]/div[3]/div[1]/div[2]/div[1]/div[2]/ul[1]/li[{a}]/dl[1]/dd[1]");
+
+                if (title == null | content == null) break;
+
+                if (keywords.Any(
+                    keyword => title.InnerText.Contains(keyword) || content.InnerText.Contains(keyword)))
+                {
+                    newsList.Add(new News
+                    {
+                        Title = title.InnerText,
+                        Url = $"https://finance.naver.com/{title.GetAttributeValue("href", "")}",
+                        ThumbnailUrl = thumbnail == null ? "" : thumbnail.GetAttributeValue("src", "")
+                    });
+                }
             }
 
-            return StatusCode(401, "Unauthorized");
+            return StatusCode(200,
+                new NewsInfoDto() {NewsList = newsList, UpdateDate = $"{DateTime.Now.AddHours(9):yyyy-MM-dd}"});
         }
     }
 }
--- src/Controllers/StockInfoController.cs
+++ src/Controllers/StockInfoController.cs
@@ -42,53 +42,49 @@ namespace StockApi.Controllers
         [HttpGet]
         public IActionResult Get([FromQuery(Name = "key")] string apiKey)
         {
-            if (apiKey == Setting.Value.ApiKey)
+            var stockInfoList = new ConcurrentBag<Stock>();
+            Parallel.ForEach(Addresses, address =>
             {
-                var stockInfoList = new ConcurrentBag<Stock>();
-                Parallel.ForEach(Addresses, address =>
+                using var wc = new WebClient
                 {
-                    using var wc = new WebClient
+                    Encoding = Encoding.UTF8,
+                    Headers =
                     {
-                        Encoding = Encoding.UTF8,
-                        Headers =
-                        {
-                            ["Accept"] =
-                                "application/x-ms-application, image/jpeg, application/xaml+xml, image/gif, image/pjpeg, application/x-ms-xbap, application/x-shockwave-flash, application/vnd.ms-excel, application/vnd.ms-powerpoint, application/msword, */*",
-                            ["User-Agent"] =
-                                "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; WOW64; Trident/4.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; MDDC)"
-                        }
-                    };
-                    var document = new HtmlDocument();
+                        ["Accept"] =
+                            "application/x-ms-application, image/jpeg, application/xaml+xml, image/gif, image/pjpeg, application/x-ms-xbap, application/x-shockwave-flash, application/vnd.ms-excel, application/vnd.ms-powerpoint, application/msword, */*",
+                        ["User-Agent"] =
+                            "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; WOW64; Trident/4.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; MDDC)"
+                    }
+                };
+                var document = new HtmlDocument();
 
-                    document.LoadHtml(wc.DownloadString(address));
+                document.LoadHtml(wc.DownloadString(address));
 
-                    var stock = new Stock
-                    {
-                        StockItem = ItemNames[Array.IndexOf(Addresses.ToArray(), address)],
-                        Price = float.Parse(document.DocumentNode.SelectSingleNode("//span[@id='last_last']").InnerText.Replace(",", "")),
-                        Fluctuation = float.Parse(document.DocumentNode
-                                                          .SelectSingleNode(
-                                                              "/html[1]/body[1]/div[5]/section[1]/div[4]/div[1]/div[1]/div[1]/div[1]/div[2]/span[2]")
-                                                          .InnerText.Replace("+", "").Replace(",", "")),
-                        FluctuationRate = float.Parse(document.DocumentNode
-                                                              .SelectSingleNode(
-                                                                  "/html[1]/body[1]/div[5]/section[1]/div[4]/div[1]/div[1]/div[1]/div[1]/div[2]/span[4]")
-                                                              .InnerText.Replace("+", "").Replace("%", "")),
-                    };
-
-                    stockInfoList.Add(stock);
-                });
-
-                var stockInfo = new StockInfoDto
+                var stock = new Stock
                 {
-                    Stocks = stockInfoList.OrderByDescending(stock => stock.StockItem).ToList(),
-                    UpdateTime = $"{DateTime.Now.AddHours(9):yyyy-MM-dd-HH-mm}"
+                    StockItem = ItemNames[Array.IndexOf(Addresses.ToArray(), address)],
+                    Price = float.Parse(document.DocumentNode.SelectSingleNode("//span[@id='last_last']").InnerText
+                        .Replace(",", "")),
+                    Fluctuation = float.Parse(document.DocumentNode
+                        .SelectSingleNode(
+                            "/html[1]/body[1]/div[5]/section[1]/div[4]/div[1]/div[1]/div[1]/div[1]/div[2]/span[2]")
+                        .InnerText.Replace("+", "").Replace(",", "")),
+                    FluctuationRate = float.Parse(document.DocumentNode
+                        .SelectSingleNode(
+                            "/html[1]/body[1]/div[5]/section[1]/div[4]/div[1]/div[1]/div[1]/div[1]/div[2]/span[4]")
+                        .InnerText.Replace("+", "").Replace("%", "")),
                 };
 
-                return StatusCode(200, stockInfo);
-            }
-            
-            return StatusCode(401, "Unauthorized");
+                stockInfoList.Add(stock);
+            });
+
+            var stockInfo = new StockInfoDto
+            {
+                Stocks = stockInfoList.OrderByDescending(stock => stock.StockItem).ToList(),
+                UpdateTime = $"{DateTime.Now.AddHours(9):yyyy-MM-dd-HH-mm}"
+            };
+
+            return StatusCode(200, stockInfo);
         }
     }
 }
